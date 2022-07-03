433cc29f75a37376ac9254eec0485b3424b8ce5d
edizzk <edizmetekurt@gmail.com>
2022-07-03 16:50:17 +0300

    create container widget for detail view

---
+++ lib/widget/clock_container.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:timezone_project/widget/text_widget.dart';
+
+Widget clockContainer(BuildContext context, String text) {
+  return Container(
+      height: MediaQuery.of(context).size.height/6,
+      width: MediaQuery.of(context).size.height/6,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(24),
+        color: Theme.of(context).bottomAppBarColor,
+      ),
+      child: Center(child: textWithSize(isBold: true, text, Theme.of(context).accentColor, 54),
+      ),
+  );
+}
