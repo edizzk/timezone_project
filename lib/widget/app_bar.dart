4588785d6a906cc6860b461f5d65e3883b3d6aef
edizzk <edizmetekurt@gmail.com>
2022-07-03 11:47:26 +0300

    add texts to app bar

--- lib/widget/app_bar.dart
+++ lib/widget/app_bar.dart
@@ -25,6 +25,7 @@ AppBar appBarMethod(double appBarHeight, String title, ThemeController themeCont
           elevation: 0.0,
           toolbarHeight: appBarHeight,
           title: Padding(padding: const EdgeInsets.only(left: 10, bottom: 10), child: appBarTextStack(title, themeController, context)),
+
           flexibleSpace: Container(
             decoration: BoxDecoration(
               borderRadius: const BorderRadius.only(bottomLeft: Radius.circular(40), bottomRight: Radius.circular(40)),
@@ -32,4 +33,31 @@ AppBar appBarMethod(double appBarHeight, String title, ThemeController themeCont
             ),
           ),
         );
+}
+
+Column appBarTextStack(String title, ThemeController themeController, BuildContext context) {
+  return Column(
+    mainAxisSize: MainAxisSize.max,
+     crossAxisAlignment: CrossAxisAlignment.start,
+     children: [
+       Text("Günaydın, Özgür!",
+          style: TextStyle(
+              color: Theme.of(context).accentColor,
+              fontSize: 18,
+              fontWeight: FontWeight.bold)),
+      Padding(
+        padding: const EdgeInsets.only(top: 5, bottom: 5),
+        child: Text("09 : 54",
+            style: TextStyle(
+                color: Theme.of(context).accentColor,
+                fontSize: 26,
+                fontWeight: FontWeight.bold)),
+      ),
+      Text("8 Haziran, Çarşamba",
+          style: TextStyle(
+              color: Theme.of(context).accentColor,
+              fontSize: 18,
+              fontWeight: FontWeight.bold)),
+    ],
+  );
 }
