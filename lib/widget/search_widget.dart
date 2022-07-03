29b166428e02e6121768139e9f083828315ec489
edizzk <edizmetekurt@gmail.com>
2022-07-03 11:45:49 +0300

    create search widget

---
+++ lib/widget/search_widget.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+import '../utils/theme.dart';
+
+Container searchWidget () {
+
+  final controller = TextEditingController();
+
+  return Container(
+    alignment: Alignment.bottomCenter,
+    height: 36,
+    width: 248,
+    decoration: BoxDecoration(
+      borderRadius: BorderRadius.circular(30),
+      color: Colors.white,
+      border: Border.all(color: Colors.black26),
+    ),
+    padding: const EdgeInsets.symmetric(horizontal: 8),
+    child: TextField(
+      controller: controller,
+      decoration: const InputDecoration(
+
+        icon: Icon(Icons.search, color: Themes.searchBarTextColor),
+      ),
+    ),
+  );
+}
