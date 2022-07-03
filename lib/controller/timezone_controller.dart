60b2ff5b0be8179300a66554facf763a99397b1c
edizzk <edizmetekurt@gmail.com>
2022-07-03 15:23:48 +0300

    fetch method for all products

--- lib/controller/timezone_controller.dart
+++ lib/controller/timezone_controller.dart
@@ -1,6 +1,8 @@
 import 'package:get/get.dart';
 import 'package:timezone_project/model/timezone_model.dart';
 
+import '../repositories/timezone_repo.dart';
+
 class TimeZoneController extends GetxController{
 
   var timezoneList = <TimeZoneModel>[].obs;
@@ -9,6 +11,20 @@ class TimeZoneController extends GetxController{
   @override
   void onInit() {
     super.onInit();
+    getAllProducts();
   }
+
+  Future<void> getAllProducts() async{
+    var product = await TimeZoneRepository().fetchProducts();
+    if(product != null)
+    {
+      timezoneList.value = product;
+      isLoading.value = false;
+    }
+    else{
+      isLoading.value = true;
+    }
+  }
+
 
 }
