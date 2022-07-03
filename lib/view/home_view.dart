acbd92ad3717b694e892304a3746393f990a4049
edizzk <edizmetekurt@gmail.com>
2022-07-03 15:30:16 +0300

    list timezones at home view

--- lib/view/home_view.dart
+++ lib/view/home_view.dart
@@ -2,13 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 import '../controller/theme_controller.dart';
+import '../controller/timezone_controller.dart';
 import '../widget/app_bar.dart';
+import '../widget/timezone_item_card.dart';
 
 class MyHomePage extends StatelessWidget {
   MyHomePage({Key? key, required this.title}) : super(key: key);
 
   final String title;
   final themeController = Get.put(ThemeController(Get.isDarkMode ? ThemeMode.light : ThemeMode.dark, Get.isDarkMode));
+  final timezoneController = Get.put(TimeZoneController());
 
   @override
   Widget build(BuildContext context) {
@@ -16,7 +19,15 @@ class MyHomePage extends StatelessWidget {
       extendBodyBehindAppBar: true,
       appBar: buildAppBar(title, context, themeController),
       backgroundColor: Theme.of(context).primaryColor,
-      body: const Center(),
+      body: Padding(
+        padding: const EdgeInsets.only(top: 10, bottom: 50),
+        child: ListView.builder(
+          itemCount: timezoneController.timezoneList.length,
+          itemBuilder: (BuildContext context, int index) {
+            return buildCard(context, index, timezoneController);
+         },
+        ),
+      )
     );
   }
 }
