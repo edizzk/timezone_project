8ed810e6792fe6a40899dc7816f2aba2a0970692
edizzk <edizmetekurt@gmail.com>
2022-07-03 15:27:43 +0300

    add color to theme

--- lib/utils/theme.dart
+++ lib/utils/theme.dart
@@ -5,13 +5,13 @@ class Themes {
     colorScheme: const ColorScheme.light(),
     primaryColor: Colors.white,
     accentColor: const Color(0xFF002359),
-    bottomAppBarColor: const Color.fromRGBO(210, 238, 255, 100),
+    bottomAppBarColor: const Color.fromRGBO(210, 238, 255, 1),
   );
   static final darkTheme = ThemeData(
     colorScheme: const ColorScheme.dark(),
     primaryColor: const Color(0xFF002359),
     accentColor: Colors.white,
-    bottomAppBarColor: const Color.fromRGBO(2, 50, 125, 100),
+    bottomAppBarColor: const Color.fromRGBO(2, 50, 125, 1),
   );
 
   static const searchBarTextColor = Color(0xFF002359);
