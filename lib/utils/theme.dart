ed66f99b32baf80060e7c9ac3cef4998b97986b9
edizzk <edizmetekurt@gmail.com>
2022-07-03 11:45:03 +0300

    create themes

--- lib/utils/theme.dart
+++ lib/utils/theme.dart
@@ -3,8 +3,16 @@ import 'package:flutter/material.dart';
 class Themes {
   static final lightTheme = ThemeData(
     colorScheme: const ColorScheme.light(),
+    primaryColor: Colors.white,
+    accentColor: const Color(0xFF002359),
+    bottomAppBarColor: const Color.fromRGBO(210, 238, 255, 100),
   );
   static final darkTheme = ThemeData(
-      colorScheme: const ColorScheme.dark(),
+    colorScheme: const ColorScheme.dark(),
+    primaryColor: const Color(0xFF002359),
+    accentColor: Colors.white,
+    bottomAppBarColor: const Color.fromRGBO(2, 50, 125, 100),
   );
+
+  static const searchBarTextColor = Color(0xFF002359);
 }
